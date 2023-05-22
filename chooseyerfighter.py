5db5879ae25404bd4081be9e2f0615c99e528969
JackoMain <134324421+JackoMain@users.noreply.github.com>
2023-05-22 15:00:10 -0400

    Add files via upload
    Very simple python function to implement in lambda.

---
+++ chooseyerfighter.py
@@ -0,0 +1,12 @@
+#import json
+
+def lambda_handler(event, context):
+
+   message = 'Choose your fighter!{}'.format(event['key1'])
+
+   return {
+
+#     'body': json.dumps not needed if json isn't called
+       'Your fighter is': message
+     
+   }
